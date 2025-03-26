d7ef27065fee16763f2b13520488dc8843eef787
João Alencar <ra197286@ucdb.br>
2025-03-26 18:28:08 -0300

    Projeto finalizado igual o do professor
    Esse é o projeto final, o professor passou esse projeto em um slide e o aluno copiou nesse código, foi realizado um teste abrindo um arquivo ARQ.txt

--- main.cpp
+++ main.cpp
@@ -7,27 +7,122 @@ using namespace std;
 
 FILE *pont_arq;
 
+//Funcao para criar novo arquivo
 bool criarNovoArquivo(char nomeArq[100]){
-    char palavra[20];
+    char palavra[20];//Variavel que será colocada no arquivo, tamanho de 50 caracteres
 
-    pont_arq = fopen(nomeArq, "W");
+    //Abrindo arquivo como modo escrita "w"
+    pont_arq = fopen(nomeArq, "w");
+
+    //Caso dê erro, mostra mensagem e retorna false
     if (pont_arq == NULL)
     {
         cout<<"Erro na abertura do arquivo";
         return false;
     }
 
-    cout<< "Escreva uma palavra para testar gravacao de arquivo";
-    
+    //coleta a palavra que o usuário digitar
+    cout<< "Escreva uma palavra para testar gravacao de arquivo: ";cin>>palavra;
+
+    //Usando fprint para armazenar a string no arquivo
+    fprintf(pont_arq, "%s", palavra);
+
+    //usando fclose para fechar o arquivo
+    fclose(pont_arq);
+
+    cout<<"Dados gravados com sucesso!";
+    return true;
+}
+
+//Funcao para acrecentar palavras no arquivo
+bool acrescentarArquivo(char nomeArquivo[500]){
+    char palavra[50]; //Variavel utilizada para manipulação dos arquivos
+
+    //Abre arquivo
+    pont_arq = fopen(nomeArquivo, "a");
+
+    //Retorna mensagem de erro
+    if (pont_arq == NULL)
+    {
+        cout<<"Erro ao abrir o arquivo";
+        return false;
+    }
+
+    //coleta palavra para ser gravada no arquivo
+    cout<<"Digite a palavra para adicionar no arquivo: ";cin>>palavra;
+
+    //grava palavra no arquivo
+    fprintf(pont_arq, "%s", palavra);
+
+    //fecha o arquivo
+    fclose(pont_arq);
+
+    cout<<"Dados gravados com sucesso!";
+    return true;
+}
+
+//Funcao para leitura de arquivos
+bool lendoDadosArquivo( char nomeArquivo[100]){
+    char c; //Armazenará cada caractere do arquivo
+
+    //Abrindo arquivo
+    pont_arq = fopen(nomeArquivo, "r");
+
+    //retorna mensagem de erro 
+    if (pont_arq == NULL){
+        cout<<"Erro ao abrir arquivo!";
+        return false;
+    }
+
+    //Mostra o contudo do arquivo
+    cout<<"CONTUDO DO ARQUIVO >> \n";
+    do{
+        //Faz a leitura do caracter no arquivo apontado por pont_arq
+        c = getc(pont_arq);
+
+        //Exibe o caracter lido na tela
+        cout<<c;
+    }while (c != EOF);
     
 }
 
 int main(){
-    char nome[50];
-    cout<<"Hello Word\n";
-    fflush(stdin);
-    cout<<"Digite uma frase";fgets(nome, 50, stdin);
-    cout<<nome;
+    char nome[100];
+    int op;
+    do
+    {
+        system("cls");//limpa o cmd
+        //mostra o menu
+        cout<<"Qual opcaoo deseja realizar?"<<endl;
+        cout<<"1 - Criar novo arquivo"<<endl;
+        cout<<"2 - Acrescentar dados no final de um arquivo"<<endl;
+        cout<<"3 - Exibir conteudo de um arquivo"<<endl;
+        cout<<"0 - Encerrar"<<endl;
+        cin>>op;
+        if( op >= 1 && op<=3){
+            cout << "informe o nome do arquivo: ", cin>>nome;    
+        }
+
+        switch (op)
+            {
+            case 1:
+                criarNovoArquivo(nome);
+                break;
+            case 2:
+                acrescentarArquivo(nome);
+                break;
+            case 3:
+                lendoDadosArquivo(nome);
+                break;
+            case 0:
+                system("pause");
+                break;;
+            default:
+                cout<<"OPÇÃO INVALIDA!";
+                system("pause");
+            }
+    } while (op != 0);
+    return EXIT_SUCCESS;
 
 getchar();
 return 0;
